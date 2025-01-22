663de15d28d388a5743b094e3cd0ae7459be2eca
Lukas Sabo <lukas.sabo@gmail.com>
2025-01-22 16:01:05 +0100

    Create Homepage and delete font-family from global.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Homepage from "@/components/Homepage";
+
 export default function Home() {
-   return <div className=""></div>;
+   return (
+      <main className="">
+         <Homepage />
+      </main>
+   );
 }
---
+++ src/components/Homepage.tsx
@@ -0,0 +1,19 @@
+import Slider from "@/components/Slider";
+
+const Homepage = () => {
+   return (
+      <div className="flex flex-col items-center justify-center gap-10 pb-10 w-full">
+         <Slider />
+         <h1 className="text-center text-xl">Mgr.Art. Helena Pohanková</h1>
+         <p className="text-center">
+            Mgr. art. Helena Pohanková je slovenská keramička s 20-ročnou skúsenosťou v
+            tomto odbore. Absolvovala Vysokú školu výtvarných umení v Bratislave (VŠVU),
+            kde získala titul Mgr. art. Počas svojej kariéry sa venovala rôznym technikám
+            keramiky a jej práce sa zameriavajú na vytváranie unikátnych, ručne vyrábaných
+            a detailne zdobených keramických diel.
+         </p>
+      </div>
+   );
+};
+
+export default Homepage;
