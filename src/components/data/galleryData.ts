fe98dd2c0f9317bb4ef172ac23bf16427bfe56f1
Lukas Sabo <lukas.sabo@gmail.com>
2025-02-18 17:16:48 +0100

    Edit gallery data

--- src/components/data/galleryData.ts
+++ src/components/data/galleryData.ts
@@ -41,64 +41,17 @@ import img41 from "@/../public/assets/images/img41.jpg";
 import img42 from "@/../public/assets/images/img42.jpg";
 import img43 from "@/../public/assets/images/img43.jpg";
 import img44 from "@/../public/assets/images/img44.jpg";
-// import thmb_img01 from "@/../public/assets/images/thumbnails/thmb_img01.jpeg";
-// import thmb_img02 from "@/../public/assets/images/thumbnails/thmb_img02.jpeg";
-// import thmb_img03 from "@/../public/assets/images/thumbnails/thmb_img03.jpeg";
-// import thmb_img04 from "@/../public/assets/images/thumbnails/thmb_img04.jpeg";
-// import thmb_img05 from "@/../public/assets/images/thumbnails/thmb_img05.jpeg";
-// import thmb_img06 from "@/../public/assets/images/thumbnails/thmb_img06.jpeg";
-// import thmb_img08 from "@/../public/assets/images/thumbnails/thmb_img08.jpeg";
-// import thmb_img09 from "@/../public/assets/images/thumbnails/thmb_img09.jpeg";
-// import thmb_img10 from "@/../public/assets/images/thumbnails/thmb_img10.jpeg";
-// import thmb_img11 from "@/../public/assets/images/thumbnails/thmb_img11.jpeg";
-// import thmb_img12 from "@/../public/assets/images/thumbnails/thmb_img12.jpeg";
-// import thmb_img13 from "@/../public/assets/images/thumbnails/thmb_img13.jpeg";
-// import thmb_img14 from "@/../public/assets/images/thumbnails/thmb_img14.jpeg";
-// import thmb_img15 from "@/../public/assets/images/thumbnails/thmb_img15.jpeg";
-// import thmb_img16 from "@/../public/assets/images/thumbnails/thmb_img16.jpeg";
-// import thmb_img17 from "@/../public/assets/images/thumbnails/thmb_img17.jpeg";
-// import thmb_img18 from "@/../public/assets/images/thumbnails/thmb_img18.jpeg";
-// import thmb_img19 from "@/../public/assets/images/thumbnails/thmb_img19.jpeg";
-// import thmb_img20 from "@/../public/assets/images/thumbnails/thmb_img20.jpeg";
-// import thmb_img21 from "@/../public/assets/images/thumbnails/thmb_img21.jpeg";
-// import thmb_img22 from "@/../public/assets/images/thumbnails/thmb_img22.jpeg";
-// import thmb_img23 from "@/../public/assets/images/thumbnails/thmb_img23.jpeg";
-// import thmb_img24 from "@/../public/assets/images/thumbnails/thmb_img24.jpeg";
-// import thmb_img25 from "@/../public/assets/images/thumbnails/thmb_img25.jpeg";
-// import thmb_img26 from "@/../public/assets/images/thumbnails/thmb_img26.jpeg";
-// import thmb_img27 from "@/../public/assets/images/thumbnails/thmb_img27.jpeg";
-// import thmb_img28 from "@/../public/assets/images/thumbnails/thmb_img28.jpeg";
-// import thmb_img29 from "@/../public/assets/images/thumbnails/thmb_img29.jpeg";
-// import thmb_img30 from "@/../public/assets/images/thumbnails/thmb_img30.jpeg";
-// import thmb_img31 from "@/../public/assets/images/thumbnails/thmb_img31.jpeg";
-// import thmb_img32 from "@/../public/assets/images/thumbnails/thmb_img32.jpeg";
-// import thmb_img33 from "@/../public/assets/images/thumbnails/thmb_img33.jpeg";
-// import thmb_img34 from "@/../public/assets/images/thumbnails/thmb_img34.jpeg";
-// import thmb_img35 from "@/../public/assets/images/thumbnails/thmb_img35.jpeg";
-// import thmb_img36 from "@/../public/assets/images/thumbnails/thmb_img36.jpeg";
-// import thmb_img37 from "@/../public/assets/images/thumbnails/thmb_img37.jpeg";
-// import thmb_img38 from "@/../public/assets/images/thumbnails/thmb_img38.jpeg";
-// import thmb_img39 from "@/../public/assets/images/thumbnails/thmb_img39.jpeg";
-// import thmb_img40 from "@/../public/assets/images/thumbnails/thmb_img40.jpeg";
-// import thmb_img41 from "@/../public/assets/images/thumbnails/thmb_img41.jpeg";
-// import thmb_img42 from "@/../public/assets/images/thumbnails/thmb_img42.jpeg";
-// import thmb_img43 from "@/../public/assets/images/thumbnails/thmb_img43.jpeg";
-// import thmb_img44 from "@/../public/assets/images/thumbnails/thmb_img44.jpeg";
 
 const galleryData = [
    {
       id: "anjel",
       name: "Anjel",
       data: [img01, img02],
-      // data_thmb: [thmb_img01, thmb_img02],
-      // position: "galeria",
    },
    {
       id: "vianocne-keramicke-gule3",
       name: "Vianočné keramické gule",
       data: [img04, img03, img05, img06, img08, img09, img09],
-      // data_thmb: [thmb_img04, thmb_img03, thmb_img05, thmb_img06, thmb_img08, thmb_img09, thmb_img09],
-      // position: "galeria",
    },
    {
       id: "miska",
@@ -114,24 +67,9 @@ const galleryData = [
          img18,
          img14,
          img19,
-         // img20,
-         // img21,
+         img20,
+         img21,
       ],
-      // data_thmb: [
-      //    thmb_img15,
-      //    thmb_img11,
-      //    thmb_img12,
-      //    thmb_img16,
-      //    thmb_img10,
-      //    thmb_img13,
-      //    thmb_img17,
-      //    thmb_img18,
-      //    thmb_img14,
-      //    thmb_img19,
-      //    thmb_img20,
-      //    thmb_img21,
-      // ],
-      // position: "galeria",
    },
    {
       id: "vianocne-ozdoby",
@@ -150,36 +88,15 @@ const galleryData = [
          img32,
          img33,
          img34,
-         // img35,
-         // img36,
-         // img37,
+         img35,
+         img36,
+         img37,
       ],
-      // data_thmb: [
-      //    thmb_img27,
-      //    thmb_img22,
-      //    thmb_img23,
-      //    thmb_img24,
-      //    thmb_img25,
-      //    thmb_img26,
-      //    thmb_img28,
-      //    thmb_img29,
-      //    thmb_img30,
-      //    thmb_img31,
-      //    thmb_img32,
-      //    thmb_img33,
-      //    thmb_img34,
-      //    thmb_img35,
-      //    thmb_img36,
-      //    thmb_img37,
-      // ],
-      // position: "galeria",
    },
    {
       id: "sperky",
       name: "Šperky",
       data: [img44, img42, img43, img38, img39, img40, img41],
-      // data_thmb: [thmb_img44, thmb_img42, thmb_img43, thmb_img38, thmb_img39, thmb_img40, thmb_img41],
-      // position: "galeria",
    },
 ];
 
