83d7cb9c45fa4c7186afc5296eb905b3721b01c9
Lukas Sabo <lukas.sabo@gmail.com>
2025-02-19 22:10:44 +0100

    Add responsive navigation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,37 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import { Facebook, Instagram } from "lucide-react";
+import { Facebook, Instagram, Menu, X } from "lucide-react";
+import { useState } from "react";
 
 const Header = () => {
+   const [isOpen, setIsOpen] = useState(false);
+
+   const toogleMenu = () => {
+      setIsOpen(!isOpen);
+
+      if (!isOpen) {
+         document.body.style.overflow = "hidden";
+      } else {
+         document.body.style.overflow = "auto";
+      }
+   };
+
    return (
-      <div className="w-full flex items-center justify-between py-5 px-4 sm:px-0 bg-white uppercase">
+      <div className="w-full flex items-center justify-between py-5 px-4 sm:px-0 uppercase">
          <Link href="/">
             <Image
                src="/logo.png"
                alt="logo"
                width="240"
                height="100"
-               className="w-24"
-               priority={true}
+               className="w-24 opacity-85 dark:invert"
             />
          </Link>
-         <div className="flex gap-10 items-center">
+
+         {/*Desktop*/}
+         <div className="sm:flex gap-10 items-center hidden">
             <Link href="/galeria">Galéria</Link>
             <Link href="/kontakt">Kontakt</Link>
             <Link
@@ -32,6 +48,45 @@ const Header = () => {
             >
                <Facebook size={20} />
             </Link>
+         </div>
+
+         {/*Mobile*/}
+         <div className="sm:hidden flex">
+            <Menu onClick={toogleMenu} />
+            {isOpen && (
+               <div className="fixed top-0 left-0 bg-background p-6 flex flex-col gap-6 w-screen h-screen overflow-hidden z-40">
+                  <Link href="/" onClick={toogleMenu}>
+                     O mne
+                  </Link>
+                  <Link href="/galeria" onClick={toogleMenu}>
+                     Galéria
+                  </Link>
+                  <Link href="/kontakt" onClick={toogleMenu}>
+                     Kontakt
+                  </Link>
+                  <div className="py-6 flex flex-col gap-6">
+                     <Link
+                        href="https://instagram.com/helena_pohankova"
+                        target="_blank"
+                        rel="noopener"
+                        className="flex flex-row items-center gap-2"
+                     >
+                        <Instagram size={18} />
+                        <span>Instagram</span>
+                     </Link>
+                     <Link
+                        href="https://www.facebook.com/heliusart"
+                        target="_blank"
+                        rel="noopener"
+                        className="flex flex-row items-center gap-2"
+                     >
+                        <Facebook size={20} />
+                        <span>Facebook</span>
+                     </Link>
+                  </div>
+                  <X onClick={toogleMenu} className="absolute top-6 right-6" />
+               </div>
+            )}
          </div>
       </div>
    );
